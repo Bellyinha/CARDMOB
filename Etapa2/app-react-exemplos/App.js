2e3e8e5a4574e71321cf8b66962345bdbd8edafe
Bellyinha <i.moraes@aluno.ifsp.edu.br>
2025-05-13 08:12:07 -0300

    View basic examples

--- Etapa2/app-react-exemplos/App.js
+++ Etapa2/app-react-exemplos/App.js
@@ -16,59 +16,37 @@ export default function App() {
     return (
         <View style={styles.container}>
             <List/> 
+            <View style={styles.redbox} />
+            <View style={styles.bluebox} />
+            <View style={styles.blackbox} />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#eff0f0',
-        marginTop: 50,
-        padding: 20,
-    },
-    text: {
-        fontSize: 30,
-        fontWeight: 'bold',
-        fontFamily: 'cursive',
-    },
-    buttonContainer: {
-        marginTop: 12,
-        flexDirection: 'row',
-        gap: 10,
-    },
-    input: {
-        height: 40,
-        borderColor: 'gray',
-        borderWidth: 1,
-        marginBottom: 10,
-        paddingHorizontal: 10,
-    },
-    list: {
-        marginTop: 20,
-    },
-    item: {
-        flexDirection: 'row',
-        alignItems: 'center',
+        flexDirection: 'column',
         justifyContent: 'space-between',
-        marginBottom: 10,
-        padding: 10,
-        backgroundColor: '#f0f0f0',
-        borderRadius: 5,
+        alignItems: 'center',
+        backgroundColor: 'lightgray',
+        height: 600,
+        marginTop: 150,
     },
-    itemText: {
-        flex: 1,
-        marginRight: 10,
+    redbox: {
+        backgroundColor: 'lightcoral',
+        width: 100,
+        height: 100,
+        borderRadius: 50,
     },
-    buttons: {
-        flexDirection: 'row',
+    bluebox: {
+        backgroundColor: 'lightblue',
+        width: 100,
+        height: 100,
     },
-    editInput: {
-        flex: 1,
-        marginRight: 10,
-        borderColor: 'gray',
-        borderWidth: 1,
-        paddingHorizontal: 10,
+    blackbox: {
+        backgroundColor: 'lightslategrey',
+        width: 100,
+        height: 100,
     },
 });
 
