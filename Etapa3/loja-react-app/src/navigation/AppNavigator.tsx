9927998f3b5d8f713983b8e4d0ace37889e12a36
Bellyinha <i.moraes@aluno.ifsp.edu.br>
2025-09-09 08:46:42 -0300

    Tela de catálogo de produtos

--- Etapa3/loja-react-app/src/navigation/AppNavigator.tsx
+++ Etapa3/loja-react-app/src/navigation/AppNavigator.tsx
@@ -10,6 +10,7 @@ import HomeScreen from '../screens/HomeScreen';
 // importar depois que implementar: DetailsScreen, SettingsScreen
 import RegisterScreen from '../screens/RegisterScreen';
 import LoginScreen from '../screens/LoginScreen';
+import CatalogScreen from '../screens/catalog/CatalogScreen';
 
 const AppStack = createNativeStackNavigator<RootStackParamList>();
 const Tab = createBottomTabNavigator<TabParamList>();
@@ -17,19 +18,26 @@ const Tab = createBottomTabNavigator<TabParamList>();
 function TabNavigator() {
     return (
         <Tab.Navigator
-            screenOptions={({ route, navigation}) => ({
+            screenOptions={({ route, navigation }) => ({
                 tabBarIcon: ({ color, focused, size }) => {
                     let iconName;
-                if (route.name === 'Home') {
-                    iconName = focused ? 'home' : 'home';
-                }
-                return <FontAwesome name={iconName} size={size} color={color} />;
+                    if (route.name === 'Catalog') {
+                        iconName = focused ? 'tags' : 'tags';
+                    }
+                    return (
+                        <FontAwesome name={iconName} size={size} color={color} />
+                    );
                 },
                 tabBarActiveTintColor: 'red',
                 tabBarInactiveTintColor: 'gray',
                 headerShown: false,
-                })}>
-            <Tab.Screen name="Home" component={HomeScreen} />
+            })}
+        >
+            <Tab.Screen 
+            name="Catalog" 
+            component={CatalogScreen} 
+            options={{title: 'Menu'}} 
+            />
             <Tab.Screen name="Settings" component={HomeScreen} />
             <Tab.Screen name="Register" component={RegisterScreen} />
         </Tab.Navigator>
@@ -53,13 +61,12 @@ function StackNavigator() {
             <AppStack.Screen
                 name="Login"
                 component={LoginScreen}
-                options={{ title: 'Acessar' }}/>
+                options={{ title: 'Acessar' }}
+            />
         </AppStack.Navigator>
     );
 }
 
 export default function AppNavigator() {
-    return (
-            <StackNavigator />
-    );
+    return <StackNavigator />;
 }
