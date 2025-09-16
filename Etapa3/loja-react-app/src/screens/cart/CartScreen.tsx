54d4c850049c3da6636dc25b4c77a5e3151ecf93
Bellyinha <i.moraes@aluno.ifsp.edu.br>
2025-09-16 10:02:51 -0300

    Inclui tela do carrinho de compras

---
+++ Etapa3/loja-react-app/src/screens/cart/CartScreen.tsx
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react';
+import { View, Text } from 'react-native';
+
+const CartScreen = ({ navigation }: any) => {
+
+    return (
+        <View>
+            <Text>Carrinho de compras</Text>
+        </View>
+    )
+}
+
+export default CartScreen;
