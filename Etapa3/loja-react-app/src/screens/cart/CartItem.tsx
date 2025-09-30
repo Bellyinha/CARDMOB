82b39e214b4e74480490ae70a1a10a00cadb0d9f
Bellyinha <i.moraes@aluno.ifsp.edu.br>
2025-09-30 09:26:50 -0300

    Estilização do carrinho - parcial

--- Etapa3/loja-react-app/src/screens/cart/CartItem.tsx
+++ Etapa3/loja-react-app/src/screens/cart/CartItem.tsx
@@ -18,20 +18,31 @@ const CartItem = ({ item }: any) => {
             <View>
                 <Text style={styles.name}>{item.name}</Text>
                 <View style={styles.quantity}>
-                    <Text style={styles.price}>R$ {(item.price * item.quantity).toFixed(2)}</Text>
+                    <Text style={styles.price}>
+                        R$ {(item.price * item.quantity).toFixed(2)}
+                    </Text>
 
-                    <TouchableOpacity onPress={() => addToCart(item, -1)} style={styles.button}>
+                    <TouchableOpacity
+                        onPress={() => addToCart(item, -1)}
+                        style={styles.button}
+                    >
                         <Text style={styles.buttonText}>-</Text>
                     </TouchableOpacity>
 
                     <Text style={styles.quantityValue}>{item.quantity}</Text>
 
-                    <TouchableOpacity onPress={() => addToCart(item)} style={styles.button}>
+                    <TouchableOpacity
+                        onPress={() => addToCart(item)}
+                        style={styles.button}
+                    >
                         <Text style={styles.buttonText}>+</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity onPress={() => handleRemove(item)} style={styles.button}>
-                        <Text style={styles.buttonText}>Remover</Text>
+                    <TouchableOpacity
+                        onPress={() => handleRemove(item)}
+                        style={styles.removeButton}
+                    >
+                        <Text style={styles.removeButtonText}>Remover</Text>
                     </TouchableOpacity>
                 </View>
             </View>
@@ -46,18 +57,49 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         padding: 10,
         borderBottomWidth: 1,
-        borderBottomColor: '#ddd',
-    },
-    image: {
-        width: '50%',
-        height: 100,
-        marginRight: 8,
         borderColor: '#ddd',
     },
-    name: {},
-    quantity: {},
-    price: {},
-    button: {},
-    quantityValue: {},
-    buttonText: {},
+    image: {
+        width: 100,
+        height: 100,
+        borderRadius: 10,
+        borderColor: '#ddd',
+        marginRight: 10,
+    },
+    name: {
+        fontSize: 16,
+    },
+    quantity: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginVertical: 5,
+    },
+    price: {
+        fontSize: 14,
+        color: '#007BFF',
+    },
+    button: {
+        backgroundColor: '#007BFF',
+        borderRadius: 5,
+        padding: 5,
+        marginHorizontal: 5,
+    },
+    buttonText: {
+        fontSize: 18,
+        color: '#fff',
+    },
+    quantityValue: {
+        marginHorizontal: 10,
+        fontSize: 16,
+    },
+    removeButton: {
+        marginLeft: 10,
+        backgroundColor: '#FF0000',
+        padding: 10,
+        borderRadius: 5,
+    },
+    removeButtonText: {
+        color: '#fff',
+        textAlign: 'center',
+    },
 });
